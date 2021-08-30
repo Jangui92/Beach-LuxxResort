316cc18c5ec234fb47495ef1911b8f7a74648c6a
Janay Anguiano <janayanguiano@gmail.com>
2021-08-30 09:41:22 -0700

    pages setup and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import logo from './logo.svg'
 import './App.css'
-import
+import Header from './components/Header'
+import Home from './pages/Home'
+import Packages from './pages/Packages'
+import Experience from './pages/Experience'
+import Reviews from './pages/Reviews'
 
 function App() {
-  
-    return( 
+  return (
     <Router>
-    <div className = "App">
-<Header />
-<Switch>
-<Route exact path = "/" component = {Home} />
-<Route path = "/packages" component = {Packages} />
-<Route path = "/reviews" component = {Reviews} />
-    </Switch>
-    </div>
-  </Router>
-    )
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/packages" component={Packages} />
+          <Route path="/experience" component={Experience} />
+          <Route path="/reviews" component={Reviews} />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
