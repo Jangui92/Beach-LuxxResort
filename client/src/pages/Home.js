530f23d6a95b00c7f1ffc8684db0035f751fd4e5
Janay Anguiano <janayanguiano@gmail.com>
2021-09-02 08:51:27 -0700

    home page picture

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,18 @@
 function Home() {
   return (
-    <section>
-      Background image
-      <div>Home Page</div>
-      <div>Accommodations</div>
-    </section>
+    <div>
+      <section>
+        Background image
+        <div>Home Page</div>
+        <div>Accommodations</div>
+      </section>
+
+      <img
+        className="welcome-picture"
+        src="https://experfly.co.uk/wp-content/uploads/2018/01/Top-5-reasons-why-you-should-honeymoon-in-Maldives-Lily-Beach-2.jpg"
+        alt="Water front"
+      ></img>
+    </div>
   )
 }
 export default Home
