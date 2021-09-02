c3bfe608d712fd396e1faf01911c571ecb224d99
Janay Anguiano <janayanguiano@gmail.com>
2021-09-02 16:35:45 -0700

    navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,44 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Nav } from 'react-bootstrap'
+import { NavBar, NavLink } from 'react-router-dom'
 
 function Header() {
   return (
-    <nav>
-      <ul className="nav-links">
-        <NavLink to="/">
-          <li>Home</li>
-        </NavLink>
-
-        <NavLink to="/packages">
-          <li>Packages</li>
-        </NavLink>
-
-        <NavLink to="/SubmitReviews">
-          <li>Submit Reviews</li>
-        </NavLink>
-      </ul>
-    </nav>
+    <ul>
+      <NavBar bg="dark" variant="dark">
+        <Container>
+          <NavBar.Brand to="/">NavBar</NavBar.Brand>
+          <Nav className="">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#packages">Packages</Nav.Link>
+            <Nav.Link href="#submit reviews">Submit Reviews</Nav.Link>
+          </Nav>
+        </Container>
+      </NavBar>
+      <br />
+      <NavBar bg="dark" variant="dark">
+        <Container>
+          <NavBar.Brand to="/">NavBar</NavBar.Brand>
+          <Nav className="">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#packages">Packages</Nav.Link>
+            <Nav.Link href="#submit reviews">Submit Reviews</Nav.Link>
+          </Nav>
+        </Container>
+      </NavBar>
+      <br />
+      <NavBar bg="dark" variant="dark">
+        <Container>
+          <NavBar.Brand to="/">NavBar</NavBar.Brand>
+          <Nav className="">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#packages">Packages</Nav.Link>
+            <Nav.Link href="#submit reviews">Submit Reviews</Nav.Link>
+          </Nav>
+        </Container>
+      </NavBar>
+      <br />
+    </ul>
   )
 }
 
