f8b70f1a92c257ed08f95780dd9e735e11f52102
Janay Anguiano <janayanguiano@gmail.com>
2021-09-02 08:26:34 -0700

    update pages and component remove packagecards

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Home from './pages/Home'
-// import Packages from './pages/Packages'
+import Packages from './pages/Packages'
 import SubmitReviews from './pages/SubmitReviews'
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
-        {/* <Route path="/packages" component={Packages} /> */}
+        <Route path="/packages" component={Packages} />
         <Route path="/SubmitReviews" component={SubmitReviews} />
       </Switch>
     </div>
--- client/src/pages/Packages.js
+++ client/src/pages/Packages.js
@@ -1,36 +1,29 @@
-// import React, { useState, useEffect } from 'react'
-// import PackagesCards from '../components/PackagesCards'
-// import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import PackagesCards from '../components/PackagesCards'
+import axios from 'axios'
 
-// const packagesCard = () => {
-//   const [packages, setPackages] = useState([])
+const packagesCard = () => {
+  const [packages, setPackages] = useState([])
 
-//   const getPackages = async () => {
-//     const res = await axios.get('http://localhost:3001/api/packages')
-//     setPackages(res.data.reviews)
-//   }
-//   useEffect(() => {
-//     getPackages()
-//   }, [])
+  const getPackages = async () => {
+    const res = await axios.get('http://localhost:3001/api/packages')
+    setPackages(res.data.reviews)
+  }
+  useEffect(() => {
+    getPackages()
+  }, [])
 
-//   return (
-//     <div>
-//       <div>
-//         <h2>Packages</h2>
-//         <section>
-//           hello
-//           {packages.map((package) => (
-//             <PackagesCards
-//               id={review.id}
-//               name={review.name}
-//               title={review.title}
-//               content={review.content}
-//               popularityRating={review.popularityRating}
-//             />
-//           ))}
-//         </section>
-//       </div>
-//     </div>
-//   )
-// }
-// export default Packages
+  return (
+    <div>
+      <div>
+        <h2>Packages</h2>
+        <section>hello</section>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+export default Packages
