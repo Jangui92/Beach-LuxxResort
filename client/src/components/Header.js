8e6c120731939a158d0b384c7943b45f1a5e5b78
Janay Anguiano <janayanguiano@gmail.com>
2021-09-03 07:55:39 -0700

    finalizing page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,29 +1,20 @@
 import React from 'react'
-import { Navbar, Container, NavLink, Nav } from 'react-bootstrap'
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 function Header() {
   return (
-    <header>
-      <Navbar sticky="top" bg="dark" variant="dark">
+    <ul>
+      <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand to="/" className="home-font">
-            {' '}
-            Beach Luxx-Resort
-          </Navbar.Brand>
-          <Nav className="navBar">
-            <NavLink to="/" className="home-font">
-              Home
-            </NavLink>
-            <NavLink to="/Packages" className="home-font">
-              Packages
-            </NavLink>
-            <NavLink to="/SubmitReviews" className="home-font">
-              Submit Reviews
-            </NavLink>
+          <Navbar.Brand to="/">Beach Luxx-Resort</Navbar.Brand>
+          <Nav className="">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/Packages">Packages</Nav.Link>
+            <Nav.Link href="/SubmitReviews">Submit Reviews</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-    </header>
+    </ul>
   )
 }
 
