bda312956bd1659d64fdde9d9246152e372cdde1
Janay Anguiano <janayanguiano@gmail.com>
2021-09-04 11:25:41 -0700

    heroku

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 let MONGODB_URI =
   process.env.NODE_ENV === 'production'
-    ? process.env.MONGODB_URL
+    ? process.env.MONGODB_URI
     : 'mongodb://127.0.0.1:27017/BeachDatabase'
 
 mongoose
