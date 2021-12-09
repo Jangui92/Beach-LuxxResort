c847c92ccd081def29a6702fd6c254a14317a4ec
Janay Anguiano <janayanguiano@gmail.com>
2021-12-09 13:09:38 -0800

    remove

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const { Router } = require('express')
 const controllers = require('../controllers')
 const router = Router()
-// This line must come before importing any instrumented module.
-const tracer = require('dd-trace').init()
 
 router.get('/', (req, res) => res.send('This is home!'))
 
@@ -14,4 +12,4 @@ router.delete('/reviews/:id', controllers.deleteReview)
 
 router.get('/packages', controllers.getAllPackages)
 
-;(module.exports = router), tracer
+module.exports = router
